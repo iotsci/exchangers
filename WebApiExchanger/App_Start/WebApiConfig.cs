ec5d27cc25ccdb1691a3721a43be72fc6b42770d
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2017-07-15 23:25:01 +0200

    Добавил Data object

--- WebApiExchanger/App_Start/WebApiConfig.cs
+++ WebApiExchanger/App_Start/WebApiConfig.cs
@@ -22,8 +22,8 @@ namespace WebApiExchanger
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
+                routeTemplate: "api/{controller}/{from}/{to}",
+                defaults: new { from = RouteParameter.Optional, to = RouteParameter.Optional }
             );
         }
     }
--- WebApiExchanger/Controllers/ExchangersController.cs
+++ WebApiExchanger/Controllers/ExchangersController.cs
@@ -10,98 +10,31 @@ using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Description;
 using WebApiExchanger.Models;
+using WebApiExchanger.DataObject;
 
 namespace WebApiExchanger.Controllers
 {
+    [Authorize]
     public class ExchangersController : ApiController
     {
         private ApplicationDbContext db = new ApplicationDbContext();
 
-        // GET: api/Exchangers
-        public IQueryable<Exchanger> GetExchangers()
+        // GET: api/Exchangers/1/2
+        [ResponseType(typeof(ExchangerDataObject))]
+        public IQueryable<ExchangerDataObject> GetExchangers(int from, int to)
         {
-            return db.Exchangers;
+            IQueryable<ExchangerDataObject> obj = db.Exchangers
+                .Where(c => c.CurrencyFrom.Id == from && c.CurrencyTo.Id == to)
+                .Select(c => new ExchangerDataObject {
+                    CurrencyFromName = c.CurrencyFrom.Name,
+                    ExchangerName = c.Name,
+                    CurrencyToName = c.CurrencyTo.Name
+                })
+                .AsQueryable();
+
+            return obj;
         }
-
-        // GET: api/Exchangers/5
-        [ResponseType(typeof(Exchanger))]
-        public async Task<IHttpActionResult> GetExchanger(int id)
-        {
-            Exchanger exchanger = await db.Exchangers.FindAsync(id);
-            if (exchanger == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(exchanger);
-        }
-
-        // PUT: api/Exchangers/5
-        [ResponseType(typeof(void))]
-        public async Task<IHttpActionResult> PutExchanger(int id, Exchanger exchanger)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            if (id != exchanger.Id)
-            {
-                return BadRequest();
-            }
-
-            db.Entry(exchanger).State = EntityState.Modified;
-
-            try
-            {
-                await db.SaveChangesAsync();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!ExchangerExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
-
-            return StatusCode(HttpStatusCode.NoContent);
-        }
-
-        // POST: api/Exchangers
-        [ResponseType(typeof(Exchanger))]
-        public async Task<IHttpActionResult> PostExchanger(Exchanger exchanger)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            db.Exchangers.Add(exchanger);
-            await db.SaveChangesAsync();
-
-            return CreatedAtRoute("DefaultApi", new { id = exchanger.Id }, exchanger);
-        }
-
-        // DELETE: api/Exchangers/5
-        [ResponseType(typeof(Exchanger))]
-        public async Task<IHttpActionResult> DeleteExchanger(int id)
-        {
-            Exchanger exchanger = await db.Exchangers.FindAsync(id);
-            if (exchanger == null)
-            {
-                return NotFound();
-            }
-
-            db.Exchangers.Remove(exchanger);
-            await db.SaveChangesAsync();
-
-            return Ok(exchanger);
-        }
-
+        
         protected override void Dispose(bool disposing)
         {
             if (disposing)
